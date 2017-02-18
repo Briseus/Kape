b75b2f92e8037aa6fe047a55b1bebf677741779c
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2017-02-18 16:06:15 +0200

    Add left side drawer

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
+import Drawer from 'material-ui/Drawer'
 
 export default class AppBarHeader extends Component {
+
+  constructor(props) {
+        super(props);
+        this.state = { open: false };
+    }
+
+    handleToggle = () => this.setState({ open: !this.state.open });
+
+    handleClose = () => this.setState({ open: false });
+
   render() {
     return (
+      <div>
       <MuiThemeProvider>
       <AppBar
+        onLeftIconButtonTouchTap={this.handleToggle}
         title="Kape"
       />
-      </MuiThemeProvider>
+       </MuiThemeProvider>
+      <Drawer 
+      docked={false}
+      open={this.state.open}
+      onRequestChange={(open) => this.setState({ open })}/>
+      
+    </div>
     )
   }
 }
