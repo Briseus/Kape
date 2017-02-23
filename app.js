0dc45700529b13f627ad0f6db69de1bbacbb6c25
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2017-02-23 16:27:58 +0200

    Clean dependecies and update node to 7.6.0

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 
 var app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(express.static('./build'));
 
 app.get('*', function(req, res) {
